4ad6ed10b34639b5c9025dce9385a3feeeb59d4d
qinbinhua601 <qinbinhua601@hotmail.com>
2019-01-08 14:51:17 +0800

    [AC] binary-subarrays-with-sum

--- binary-subarrays-with-sum.js
+++ binary-subarrays-with-sum.js
@@ -4,5 +4,24 @@
  * @return {number}
  */
 var numSubarraysWithSum = function(A, S) {
-
+  if (A.length < S) {
+      return 0;
+  }
+  var count = 0;
+  var start = 0;
+  while(start < A.length) {
+      var sum = 0;
+      for (var i = start; i < A.length; i++) {
+          sum += A[i]
+          if (sum === S) {
+              count++
+          } else {
+              if (sum > S) {
+                  break;
+              }
+          }
+      }
+      start++;
+  }
+  return count;
 };
